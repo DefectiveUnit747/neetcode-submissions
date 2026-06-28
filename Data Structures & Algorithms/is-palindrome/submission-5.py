e05de3419affba6687ebf56cf50550b38eed9868
Christopher George <124778768+DefectiveUnit747@users.noreply.github.com>
2026-06-28 22:04:45 +0100

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        charsToRemove = []
+        for letter in s:
+            if not letter.isalnum():
+                charsToRemove.append(letter)
+        print(charsToRemove)
+        for character in charsToRemove:
+            s = s.replace(character, "")
+        s = s.lower()
+
+        length = len(s)
+        
+        frontPointer = 0
+        endPointer = length - 1
+
+        middleIndex = (length // 2) - 1
+        while frontPointer <= middleIndex and endPointer >= middleIndex:
+            if s[frontPointer] != s[endPointer]:
+                return False
+            frontPointer += 1
+            endPointer -= 1
+        return True
+        
+
